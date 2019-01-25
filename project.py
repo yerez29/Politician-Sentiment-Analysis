eb88772be2dd42c66af74f48e508ed8beb94f0eb
Yair Erez <yerez.29@cs.huji.ac.il>
2019-01-25 13:17:08 +0200

    start working on ML and location based opinions

--- project.py
+++ project.py
@@ -4,7 +4,8 @@ import nltk
 import pandas as pd
 from textblob import TextBlob
 import matplotlib.pyplot as plt
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from math import ceil
+from textblob.classifiers import NaiveBayesClassifier
 
 
 def getArticles(keywords_list, articles_file_name):
@@ -32,6 +33,7 @@ def getComments(keywords_articles_ids, comments_file_name):
     :return:
     """
     relevant_comments = []
+    comments_indices = []
     df = pd.read_csv(comments_file_name)
     ids_column = df.articleID
     comments_column = df.commentBody
@@ -39,14 +41,20 @@ def getComments(keywords_articles_ids, comments_file_name):
     for i in range(num_of_ids):
         if ids_column[i] in keywords_articles_ids:
             relevant_comments.append(comments_column[i])
-    return relevant_comments
+            comments_indices.append(i)
+    return relevant_comments, comments_indices
 
+def percent(part, whole):
 
-def getMarks(relevant_comments, comments_file_name):
+    return (part/whole) * 100
+
+def getGeneralOpinions(relevant_comments, comments_file_name):
     """
     :param comments:
     :return:
     """
+    train = []
+    train_counter = 0
     general_mark = 0
     very_positive_mark = 0
     slightly_positive_mark = 0
@@ -55,32 +63,67 @@ def getMarks(relevant_comments, comments_file_name):
     natural_mark = 0
     geo_location_marks = {}
     num_of_comments = len(relevant_comments)
+    num_of_trains = ceil(num_of_comments / 100)
     date = comments_file_name[8:len(comments_file_name) - 4]
     for comment in relevant_comments:
-        blob = TextBlob(comment)
-        cur_average_mark = blob.sentences[0].sentiment.polarity
+        analysis = TextBlob(comment)
+        cur_average_mark = analysis.sentences[0].sentiment.polarity
         general_mark += cur_average_mark
-        if cur_average_mark > 0.05:
+        if cur_average_mark > 0.00:
             if cur_average_mark > 0.3:
                 very_positive_mark += 1
+                if train_counter <= num_of_trains:
+                    train.append((str(analysis.sentences[0]), "very pos"))
             else:
                 slightly_positive_mark += 1
-        elif cur_average_mark < -0.05:
+                if train_counter <= num_of_trains:
+                    train.append((str(analysis.sentences[0]), "slightly pos"))
+        elif cur_average_mark < 0.00:
             if cur_average_mark < -0.3:
                 very_negative_mark += 1
+                if train_counter <= num_of_trains:
+                    train.append((str(analysis.sentences[0]), "very neg"))
             else:
                 slightly_negative_mark += 1
+                if train_counter <= num_of_trains:
+                    train.append((str(analysis.sentences[0]), "slightly neg"))
         else:
             natural_mark += 1
+            if train_counter <= num_of_trains:
+                train.append((str(analysis.sentences[0]), "natural"))
+        train_counter += 1
     general_mark /= num_of_comments
     print("total normalized mark in", date, "is", general_mark)
+    very_positive_mark = percent(very_positive_mark, num_of_comments)
+    slightly_positive_mark = percent(slightly_positive_mark, num_of_comments)
+    slightly_negative_mark = percent(slightly_negative_mark, num_of_comments)
+    very_negative_mark = percent(very_negative_mark, num_of_comments)
+    natural_mark = percent(natural_mark, num_of_comments)
     labels = 'Very positive', 'Slightly positive', 'Slightly negative', 'Very negative', 'Natural'
-    sizes = [(very_positive_mark / num_of_comments) * 100, (slightly_positive_mark / num_of_comments) * 100, (slightly_negative_mark / num_of_comments) * 100, (very_negative_mark / num_of_comments) * 100, (natural_mark / num_of_comments) * 100]
-    fig1, ax1 = plt.subplots()
-    ax1.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
-    ax1.axis('equal')
+    sizes = [very_positive_mark, slightly_positive_mark, slightly_negative_mark, very_negative_mark, natural_mark]
+    plt.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
+    plt.title("Opinion distribution in " + date)
+    plt.axis('equal')
+    # plt.tight_layout()
     plt.show()
     plt.clf()
+    print(train)
+    return train
+
+def getLocationBasedOpinions(relevantComments, comments_indices):
+
+
+
+def getClassificationsByMl(relevant_comments, comments_file_name, train):
+
+    cl = NaiveBayesClassifier(train)
+    for comment in relevant_comments:
+        blob = TextBlob(comment)
+        analysis = TextBlob(str(blob.sentences[0]), classifier=cl)
+        print(str(blob.sentences[0]))
+        print("\n")
+        print(analysis.classify())
+        print("\n")
 
 
 keywords_list = ["Trump", 'Donald', "trump", "donald", "TRUMP", "DONALD", "Trump, Donald J", "Donald Trump", "Donald John Trump", "Donald J. Trump"]
@@ -88,53 +131,63 @@ keywords_list = ["Trump", 'Donald', "trump", "donald", "TRUMP", "DONALD", "Trump
 articles_file_name = "ArticlesJan2017.csv"
 keywords_articles_ids = getArticles(keywords_list, articles_file_name)
 comments_file_name = "CommentsJan2017.csv"
-relevant_comments = getComments(keywords_articles_ids, comments_file_name)
-getMarks(relevant_comments, comments_file_name)
+relevant_comments, comments_indices = getComments(keywords_articles_ids, comments_file_name)
+train = getGeneralOpinions(relevant_comments, comments_file_name)
+getLocationBasedOpinions(relevant_comments, comments_indices)
+getClassificationsByMl(relevant_comments, comments_file_name, train)
 # FEB 2017
 articles_file_name = "ArticlesFeb2017.csv"
 keywords_articles_ids = getArticles(keywords_list, articles_file_name)
 comments_file_name = "CommentsFeb2017.csv"
-relevant_comments = getComments(keywords_articles_ids, comments_file_name)
-getMarks(relevant_comments, comments_file_name)
+relevant_comments, comments_indices = getComments(keywords_articles_ids, comments_file_name)
+getGeneralOpinions(relevant_comments, comments_file_name)
+getLocationBasedOpinions(relevant_comments, comments_indices)
 # MARCH 2017
 articles_file_name = "ArticlesMarch2017.csv"
 keywords_articles_ids = getArticles(keywords_list, articles_file_name)
 comments_file_name = "CommentsMarch2017.csv"
-relevant_comments = getComments(keywords_articles_ids, comments_file_name)
-getMarks(relevant_comments, comments_file_name)
+relevant_comments, comments_indices = getComments(keywords_articles_ids, comments_file_name)
+getGeneralOpinions(relevant_comments, comments_file_name)
+getLocationBasedOpinions(relevant_comments, comments_indices)
 # APRIL 2017
 articles_file_name = "ArticlesApril2017.csv"
 keywords_articles_ids = getArticles(keywords_list, articles_file_name)
 comments_file_name = "CommentsApril2017.csv"
-relevant_comments = getComments(keywords_articles_ids, comments_file_name)
-getMarks(relevant_comments, comments_file_name)
+relevant_comments, comments_indices = getComments(keywords_articles_ids, comments_file_name)
+getGeneralOpinions(relevant_comments, comments_file_name)
+getLocationBasedOpinions(relevant_comments, comments_indices)
 # MAY 2017
 articles_file_name = "ArticlesMay2017.csv"
 keywords_articles_ids = getArticles(keywords_list, articles_file_name)
 comments_file_name = "CommentsMay2017.csv"
-relevant_comments = getComments(keywords_articles_ids, comments_file_name)
-getMarks(relevant_comments, comments_file_name)
+relevant_comments, comments_indices = getComments(keywords_articles_ids, comments_file_name)
+getGeneralOpinions(relevant_comments, comments_file_name)
+getLocationBasedOpinions(relevant_comments, comments_indices)
 # JAN 2018
 articles_file_name = "ArticlesJan2018.csv"
 keywords_articles_ids = getArticles(keywords_list, articles_file_name)
 comments_file_name = "CommentsJan2018.csv"
-relevant_comments = getComments(keywords_articles_ids, comments_file_name)
-getMarks(relevant_comments, comments_file_name)
+relevant_comments, comments_indices = getComments(keywords_articles_ids, comments_file_name)
+getGeneralOpinions(relevant_comments, comments_file_name)
+getLocationBasedOpinions(relevant_comments, comments_indices)
 # FEB 2018
 articles_file_name = "ArticlesFeb2018.csv"
 keywords_articles_ids = getArticles(keywords_list, articles_file_name)
 comments_file_name = "CommentsFeb2018.csv"
-relevant_comments = getComments(keywords_articles_ids, comments_file_name)
-getMarks(relevant_comments, comments_file_name)
+relevant_comments, comments_indices = getComments(keywords_articles_ids, comments_file_name)
+getGeneralOpinions(relevant_comments, comments_file_name)
+getLocationBasedOpinions(relevant_comments, comments_indices)
 # MARCH 2018
 articles_file_name = "ArticlesMarch2018.csv"
 keywords_articles_ids = getArticles(keywords_list, articles_file_name)
 comments_file_name = "CommentsMarch2018.csv"
-relevant_comments = getComments(keywords_articles_ids, comments_file_name)
-getMarks(relevant_comments, comments_file_name)
+relevant_comments, comments_indices = getComments(keywords_articles_ids, comments_file_name)
+getGeneralOpinions(relevant_comments, comments_file_name)
+getLocationBasedOpinions(relevant_comments, comments_indices)
 # APRIL 2018
 articles_file_name = "ArticlesApril2018.csv"
 keywords_articles_ids = getArticles(keywords_list, articles_file_name)
 comments_file_name = "CommentsApril2018.csv"
-relevant_comments = getComments(keywords_articles_ids, comments_file_name)
-getMarks(relevant_comments, comments_file_name)
+relevant_comments, comments_indices = getComments(keywords_articles_ids, comments_file_name)
+getGeneralOpinions(relevant_comments, comments_file_name)
+getLocationBasedOpinions(relevant_comments, comments_indices)
